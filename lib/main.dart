d12d312dd296b859879a6e3fa3168f0a0e30b7e3
Youssef Salah72 <yzmn2003@gmail.com>
2026-04-30 22:28:49 +0300

    create Mini Program

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
-void main(){
-  String name ;
-  int age ;
-  String country ;
-  int department ; 
+void main() {
+  String name, country, selectedDepartment = '';
+  int age;
+  int department;
   print('Enter your name :');
   name = stdin.readLineSync()!;
   print('Enter your age :');
@@ -13,15 +12,25 @@ void main(){
   country = stdin.readLineSync()!;
   print('Enter your Section :\n1-Soft\n2-Genral\n3-Bio');
   department = int.parse(stdin.readLineSync()!);
-  if(department==1 || department==2){
-print('Your Departm 14000');
-  }else if(department==3){
-    print('Your Department 1000');
-  }else{
-  print('Enter your Section :\n1-Soft\n2-Genral\n3-Bio');
+  switch (department) {
+    case 1:
+      print('Your Departm 14000');
+      selectedDepartment = 'Soft';
+      break;
+    case 2:
+      print('Your Departm 14000');
+      selectedDepartment = 'Bio';
+      break;
+    case 3:
+      print('Your Department 1000');
+      selectedDepartment = 'General';
+      break;
+    default:
+      print('Enter your Section :\n1-Soft\n2-Genral\n3-Bio');
   }
+  ;
   print('======= Your Information =======');
-  print(' Name : $name \n Age : $age \n Country : $country \n Department : $department');
-
-
+  print(
+    ' Name : $name \n Age : $age \n Country : $country \n Department : $selectedDepartment',
+  );
 }
